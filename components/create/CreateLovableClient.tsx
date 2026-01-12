34f0213cda3f3caacb67e01440c2a91095545d7a
tipote <blagardette@gmail.com>
2026-01-12 10:41:05 +0100

    generate content

--- components/create/CreateLovableClient.tsx
+++ components/create/CreateLovableClient.tsx
@@ -91,7 +91,7 @@ export default function CreateLovableClient() {
   const handleGenerate = async (params: any): Promise<string> => {
     setIsGenerating(true);
     try {
-      const res = await fetch("/api/generate-content", {
+      const res = await fetch("/api/content/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(params),
